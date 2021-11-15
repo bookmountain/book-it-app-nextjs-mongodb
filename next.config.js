7cea3eabbba313e6f25b27d1804397e0c812871d
Sam Chang <bookmountain0222@gmail.com>
2021-11-16 00:13:34 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    DB_LOCAL_URI:
+      "mongodb+srv://sam:O8PU5KbOPrkHP7WP@cluster0.nskku.mongodb.net/bookit?retryWrites=true&w=majority",
+  },
+};
