661b67c4718c8778aa66ff7800f46c25289e08d1
Sam Chang <bookmountain0222@gmail.com>
2021-11-17 22:16:55 +0800

    Update: add pagination

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,12 +1,25 @@
 import Room from "../models/room";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors";
+import ApiFeature from "../utils/apiFeature";
 
 const getAllRooms = catchAsyncErrors(async (req, res) => {
-  const rooms = await Room.find();
+  const resPerPage = 4;
+  const roomsCount = await Room.countDocuments();
+
+  const apiFeatures = new ApiFeature(Room.find(), req.query).search().filter();
+
+  let rooms = await apiFeatures.query;
+  let filteredRoomsCount = rooms.length;
+
+  apiFeatures.pagination(resPerPage);
+  rooms = await apiFeatures.query.clone();
+
   res.status(200).json({
     success: true,
-    count: rooms.length,
+    roomsCount,
+    resPerPage,
+    filteredRoomsCount,
     rooms,
   });
 });
